f425921b8edb87bffac486ca171e4e5a7af302bf
ninjapiraatti <ninjapiraatti@DESKTOP-UGBSIUI.localdomain>
2023-07-05 11:24:52 +0300

    Update .gitignore, add basic DB connection

--- main.go
+++ main.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"os"
+
+	"github.com/go-pg/pg/v10"
+	"github.com/joho/godotenv"
 )
 
 type Book struct {
@@ -19,6 +24,32 @@ func getBooks(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+		return
+	}
+
+	dbPassword := os.Getenv("DB_PASSWORD")
+
+	pgDB := pg.Connect(&pg.Options{
+		Addr:     "localhost:5432",
+		User:     "postgres",
+		Password: dbPassword,
+		Database: "aninmals",
+	})
+
+	defer pgDB.Close()
+
+	// test the connection
+	_, connErr := pgDB.Exec("SELECT 1")
+	if connErr != nil {
+		fmt.Println(connErr)
+		return
+	}
+
+	fmt.Println("Connected successfully!")
+
 	http.HandleFunc("/books", getBooks)
 
 	books = append(books, Book{ID: "1", Title: "Book One", Author: "Author One"})
